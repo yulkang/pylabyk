01d40095e482327ac52670f1c44d05d97f533189
YulKang <yul.kang.on@gmail.com>
2019-04-30 10:15:51 +0100

    circular params added to BoundedModule; passes tests

--- yktorch.py
+++ yktorch.py
@@ -10,6 +10,7 @@ class BoundedModule(nn.Module):
         super(BoundedModule, self).__init__()
         self._params_bounded = {} # {name:(lb, ub)}
         self._params_probability = {} # {name:probdim}
+        self._params_circular = {}
         self.epsilon = 1e-6
 
     def setslice(self, name, index, value):
@@ -18,13 +19,13 @@ class BoundedModule(nn.Module):
         self.__setattr__(name, v)
 
     # Bounded parameters whose elements are individually bounded
-    def register_bounded_parameter(self, name, data, lb=0, ub=1):
+    def register_bounded_parameter(self, name, data, lb=0., ub=1.):
         data = enforce_float_tensor(data)
         self._params_bounded[name] = {'lb':lb, 'ub':ub}
-        param = self.bounded_data2param(data, lb, ub)
+        param = self._bounded_data2param(data, lb, ub)
         self.register_parameter('_bounded_' + name, nn.Parameter(param))
 
-    def bounded_data2param(self, data, lb=0, ub=1):
+    def _bounded_data2param(self, data, lb=0., ub=1.):
         data = enforce_float_tensor(data)
         if lb is None and ub is None:# Unbounded
             return data
@@ -38,9 +39,9 @@ class BoundedModule(nn.Module):
             data[data < lb + self.epsilon] = lb + self.epsilon
             data[data > ub - self.epsilon] = ub - self.epsilon
             p = (data - lb) / (ub - lb)
-            return torch.log(p) - torch.log(1 - p)
+            return torch.log(p) - torch.log(1. - p)
 
-    def bounded_param2data(self, param, lb=0, ub=1):
+    def _bounded_param2data(self, param, lb=0., ub=1.):
         param = enforce_float_tensor(param)
         if lb is None and ub is None: # Unbounded
             return param
@@ -58,14 +59,14 @@ class BoundedModule(nn.Module):
                                        probdim=0) -> None:
         self._params_probability[name] = {'probdim':probdim}
         self.register_parameter('_conf_' + name,
-                                nn.Parameter(self.prob2conf(prob)))
+                                nn.Parameter(self._prob2conf(prob)))
 
-    def prob2conf(self, prob, probdim=0):
+    def _prob2conf(self, prob, probdim=0):
         prob = enforce_float_tensor(prob)
         incl = prob < self.epsilon
 
         prob[prob < self.epsilon] = self.epsilon
-        prob[prob > 1 - self.epsilon] = 1 - self.epsilon
+        prob[prob > 1. - self.epsilon] = 1. - self.epsilon
         prob = prob / torch.sum(prob, dim=probdim, keepdim=True)
 
         # ndim = prob.ndimension()
@@ -82,7 +83,7 @@ class BoundedModule(nn.Module):
 
         return torch.log(prob)
 
-    def conf2prob(self, conf, probdim=0):
+    def _conf2prob(self, conf, probdim=0):
         # ndim = conf.ndimension()
         # ix_conf = []
         # for dim in range(ndim):
@@ -95,6 +96,21 @@ class BoundedModule(nn.Module):
         # return prob
 
         return F.softmax(enforce_float_tensor(conf), dim=probdim)
+
+    # Circular parameters: limited to (param - lb) % (ub - lb)
+    def register_circular_parameter(self, name, data, lb=0., ub=1.):
+        data = enforce_float_tensor(data)
+        self._params_circular[name] = {'lb':lb, 'ub':ub}
+        param = self._circular_data2param(data, lb, ub)
+        self.register_parameter('_circular_' + name, nn.Parameter(param))
+
+    def _circular_data2param(self, data, lb=0., ub=1.):
+        data = enforce_float_tensor(data)
+        return (data - lb) / (ub - lb) % 1.
+
+    def _circular_param2data(self, param, lb=0., ub=1.):
+        param = enforce_float_tensor(param)
+        return param * (ub - lb) + lb
 
     # Convenience function
     @property
@@ -114,33 +130,36 @@ class BoundedModule(nn.Module):
 
     # Get/set
     def __getattr__(self, item):
-        if item in ['_params_bounded', '_params_probability']:
+        if item in ['_params_bounded', '_params_probability',
+                    '_params_circular']:
             try:
                 return super(BoundedModule, self).__getattribute__(item)
             except:
                 return {}
 
         if hasattr(self, '_params_bounded'):
-            _params_bounded = self.__dict__['_params_bounded']
-            # _params_bounded = super().__getattribute__(
-            #     '_params_bounded'
-            # )
-            if item in _params_bounded.keys():
-                info = _params_bounded[item]
+            _params = self.__dict__['_params_bounded']
+            if item in _params:
+                info = _params[item]
                 param = super().__getattr__(
                     '_bounded_' + item)
-                return self.bounded_param2data(param, **info)
+                return self._bounded_param2data(param, **info)
 
         if hasattr(self, '_params_probability'):
-            _params_probability = self.__dict__['_params_probability']
-            # _params_probability = super().__getattribute__(
-            #     '_params_probability'
-            # )
-            if item in _params_probability.keys():
-                info = _params_probability[item]
+            _params = self.__dict__['_params_probability']
+            if item in _params:
+                info = _params[item]
                 param = super().__getattr__(
                     '_conf_' + item)
-                return self.conf2prob(param, **info)
+                return self._conf2prob(param, **info)
+
+        if hasattr(self, '_params_circular'):
+            _params = self.__dict__['_params_circular']
+            if item in _params:
+                info = _params[item]
+                param = super().__getattr__(
+                    '_circular_' + item)
+                return self._circular_param2data(param, **info)
 
         return super().__getattr__(item)
 
@@ -149,22 +168,32 @@ class BoundedModule(nn.Module):
             self.__dict__[item] = value
 
         if hasattr(self, '_params_bounded'):
-            _params_bounded = self._params_bounded
-            if item in _params_bounded.keys():
-                info = _params_bounded[item]
-                param = self.bounded_data2param(value, **info)
+            _params = self._params_bounded
+            if item in _params:
+                info = _params[item]
+                param = self._bounded_data2param(value, **info)
                 super().__setattr__(
                     '_bounded_' + item, nn.Parameter(param)
                 )
                 return
 
         if hasattr(self, '_params_probability'):
-            _params_probability = self._params_probability
-            if item in _params_probability:
-                info = _params_probability[item]
-                param = self.prob2conf(value, **info)
+            _params = self._params_probability
+            if item in _params:
+                info = _params[item]
+                param = self._prob2conf(value, **info)
                 super().__setattr__(
                     '_conf_' + item, nn.Parameter(param)
+                )
+                return
+
+        if hasattr(self, '_params_circular'):
+            _params = self._params_circular
+            if item in _params:
+                info = _params[item]
+                param = self._circular_data2param(value, **info)
+                super().__setattr__(
+                    '_circular_' + item, nn.Parameter(param)
                 )
                 return
 
@@ -193,8 +222,8 @@ class TestBoundedModule(unittest.TestCase):
                 if lb is not None and ub is not None and lb >= ub:
                     continue
 
-                param = bound.bounded_data2param(data, lb, ub)
-                data1 = bound.bounded_param2data(param, lb, ub)
+                param = bound._bounded_data2param(data, lb, ub)
+                data1 = bound._bounded_param2data(param, lb, ub)
                 min_err = torch.min(torch.abs(data1 - data))
                 self.assertTrue(
                     min_err <= bound.epsilon * 10,
@@ -226,6 +255,36 @@ class TestBoundedModule(unittest.TestCase):
                         min_err, none2str(lb), none2str(ub))
                 )
 
+    def test_register_circular_parameter(self):
+        def none2str(v):
+            if v is None:
+                return 'None'
+            else:
+                return '%d' % v
+
+        bound = BoundedModule()
+        data = torch.zeros(2, 3)
+        for lb in [0, 5, 20]:
+            for scale in [1, 5, 20]:
+                for offset in [0, 0.01, 0.5, scale]:
+                    bound.register_circular_parameter('circular0',
+                                                      data + lb
+                                                      + offset, # w/o + scale
+                                                      lb, lb + scale)
+                    bound.register_circular_parameter('circular1',
+                                                      data + lb
+                                                      + scale + offset,
+                                                      lb, lb + scale)
+                    data0 = bound.circular0
+                    data1 = bound.circular1
+
+                    min_err = torch.min(torch.abs(data1 - data0))
+                    self.assertTrue(
+                        min_err <= bound.epsilon * 10,
+                        'error of %g with lb=%s, scale=%s, offset=%g' % (
+                            min_err, lb, scale, offset)
+                    )
+
     def test_prob_data2param2data(self):
         bound = BoundedModule()
         for p in [0, 0.5, 1]:
@@ -233,8 +292,8 @@ class TestBoundedModule(unittest.TestCase):
                 torch.zeros(1, 3) + p,
                 torch.ones(1, 3) - p
                 ], dim=0)
-            param = bound.prob2conf(data)
-            data1 = bound.conf2prob(param)
+            param = bound._prob2conf(data)
+            data1 = bound._conf2prob(param)
 
             min_err = torch.min(torch.abs(data1 - data))
             self.assertTrue(
@@ -277,11 +336,18 @@ if __name__ == 'main':
     bound.bounded = [1, 0.7, 0]
     print(bound.bounded)
 
+    bound.register_circular_parameter('circular', [0, 0.5, 1, 1.5, 2], 0, 1)
+    print(bound.circular)
+
+    bound.circular = [1, 0.7, 0, -0.5]
+    print(bound.circular)
+
     test = TestBoundedModule()
     test.test_bounded_data2param2data()
     test.test_prob_data2param2data()
     test.test_register_bounded_parameter()
     test.test_register_probability_parameter()
+    test.test_register_circular_parameter()
 
     #%%
     res = unittest.defaultTestLoader.loadTestsFromTestCase(TestBoundedModule).run(
@@ -290,3 +356,5 @@ if __name__ == 'main':
     print(res.errors)
     print('failures:')
     print(res.failures)
+
+    #%%
