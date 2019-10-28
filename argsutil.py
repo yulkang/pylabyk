0ce44f9c5bfbb5bdd55995f4e42f086004fc0cc8
YulKang <yul.kang.on@gmail.com>
2019-10-28 09:28:13 +0000

    made kwdefault use OrderedDict; added kwdef that allows more options

--- argsutil.py
+++ argsutil.py
@@ -6,6 +6,8 @@ Created on Fri Oct 12 10:58:48 2018
 @author: yulkang
 """
 
+from collections import OrderedDict
+
 def varargin2props(obj, kw, skip_absent=True, error_absent=False):
     keys0 = obj.__dict__.keys()
     for key in kw.keys():
@@ -17,9 +19,30 @@ def varargin2props(obj, kw, skip_absent=True, error_absent=False):
             obj.__dict__[key] = kw[key]
 
 def kwdefault(kw_given, **kw_default):
+    kw_default = OrderedDict(kw_default)
     for k in kw_given:
         kw_default[k] = kw_given[k]
     return kw_default
+
+def kwdef(kw_given, kw_def=None,
+          sort_def=False, sort_given=False, def_bef_given=True):
+    if kw_def is None:
+        kw_def = {}
+
+    kw_def = OrderedDict(kw_def)
+    kw_given = OrderedDict(kw_given)
+    if sort_def:
+        kw_def = OrderedDict(sorted(kw_def.items()))
+    if sort_given:
+        kw_def = OrderedDict(sorted(kw_given.items()))
+
+    if def_bef_given:
+        for k in kw_given:
+            kw_def[k] = kw_given[k]
+    else:
+        for k in kw_def:
+            kw_given[k] = kw_def[k]
+    return kw_def
 
 def dict2fname(d, skip_None=True):
     def to_include(k):
