94d9fd1f4922ebed8cfb03b262c4dbbcc3182961
YulKang <yul.kang.on@gmail.com>
2019-12-10 10:35:15 +0000

    added example_* functions to cacheutil

--- cacheutil.py
+++ cacheutil.py
@@ -1,7 +1,12 @@
+"""
+See examples in the docstring of Cache (example_* at the bottom of this module)
+"""
+
 import os
 from . import zipPickle
-from collections import OrderedDict
+from collections import OrderedDict as odict
 from .argsutil import dict2fname
+from .argsutil import kwdef
 
 def dict_except(d, keys_to_excl):
     return {k:d[k] for k in d if k not in keys_to_excl}
@@ -23,28 +28,11 @@ class Cache(object):
     """
     Caches to file for fast retrieval.
 
-    EXAMPLE:
-    def fun(test_param=1, to_recompute=False):
-        cache = Cache(
-            os.path.join('cache.pkl'),
-            locals()
-        )
-        if cache.exists() and not to_recompute:
-            a, b = cache.getdict([
-                'a', 'b'
-            ])
-        else:
-            a = 10 * test_param
-            b = 100 * test_param
+    EXAMPLE 1 - use locals() to name the key inside a fixed cache file.
+    See example_cache_fixed_file()
 
-            cache.update_dict({
-                'a': a,
-                'b': b
-            })
-        print('test_param: %d, a: %d, b:%d' % (test_param, a, b))
-
-    for test_param_main in range(5):
-        fun(test_param_main)
+    EXAMPLE 2 - use custom cache file name (good for parallel execution):
+    See example_cache_custom_file()
     """
     def __init__(self, fullpath='cache.pkl.zip', key=None, verbose=True,
                  ignore_key=False):
@@ -176,6 +164,66 @@ class Cache(object):
     def __del__(self):
         if self.to_save:
             self.save()
+
+def example_cache_fixed_file():
+    def fun(test_param=1, to_recompute=False):
+        cache = Cache(
+            os.path.join('cache.pkl.zip'),
+            locals()
+        )
+        if cache.exists() and not to_recompute:
+            a, b = cache.getdict([
+                'a', 'b'
+            ])
+        else:
+            a = 10 * test_param
+            b = 100 * test_param
+
+            cache.update_dict({
+                'a': a,
+                'b': b
+            })
+        print('test_param: %d, a: %d, b:%d' % (test_param, a, b))
+
+    for test_param_main in range(5):
+        fun(test_param_main)
+
+def example_cache_custom_file():
+    pth_cache = 'Data/cache_example'
+    def get_cache(subj, kw_model=None):
+        # name_cache: to name corresponding figures
+        if kw_model is None:
+            kw_model = {}
+        name_cache = dict2fname(kwdef( # cacheutil.dict2fname, argsutil.kwdef
+            kw_model,
+            odict([
+                ('fit', 'gaze_emission'),
+                ('sbj', subj),
+            ])
+        ))
+        fit_file = os.path.join(pth_cache, name_cache + '.pkl.zip')
+        cache = Cache(fit_file) # cacheutil.Cache
+        return cache, name_cache
+
+    # Key to find the cache
+    subj = 'test_cache'
+    kw_model = odict([
+        ('field1', 1),
+        ('field2', 'value2')
+    ])
+
+    # Set cache
+    cache, name_cache = get_cache(subj, kw_model)
+    cache.set({
+        'key1': 10,
+        'key2': 'value20'
+    })
+    cache.save()
+
+    # Load cache
+    cache2, name_cache = get_cache(subj, kw_model)
+    val1, val2 = cache2.getdict(['key1', 'key2'])
+    print(val1, val2)
 
 # if __name__ == '__main__':
 #     def fun(test_param=1, to_recompute=False):
