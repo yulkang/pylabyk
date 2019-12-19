abf1ed1119c0f56fdff9d9f16a4e37521698a60b
YulKang <yul.kang.on@gmail.com>
2019-12-19 15:56:47 +0000

    normrnd and mvnrnd debugged to default to sample_shape=()

--- numpytorch.py
+++ numpytorch.py
@@ -547,13 +547,18 @@ def rand(shape, low=0, high=1):
     d = Uniform(low=low, high=high)
     return d.rsample(shape)
 
-def mvnrnd(mu, sigma, sample_shape=torch.Size([])):
+def mvnrnd(mu, sigma, sample_shape=()):
     d = MultivariateNormal(loc=mu, covariance_matrix=sigma)
     return d.rsample(sample_shape)
 
-def normrnd(mu=0., sigma=1., sample_shape=(1,)):
+def normrnd(mu=0., sigma=1., sample_shape=()):
     d = Normal(loc=mu, scale=sigma)
     return d.rsample(sample_shape)
+
+def categrnd(probs=None, logits=None, sample_shape=()):
+    return torch.distributions.Categorical(
+        probs=probs, logits=logits
+    ).rsample(sample_shape=sample_shape)
 
 def mvnpdf_log(x, mu=None, sigma=None):
     if mu is None:
