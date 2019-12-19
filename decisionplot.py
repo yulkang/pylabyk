16c9af8121d3f7679740d9a32a690308f482fb08
YulKang <yul.kang.on@gmail.com>
2019-12-19 12:20:28 +0000

    decisionplot now uses Decision() class to share common settings like dt, nt

--- decisionplot.py
+++ decisionplot.py
@@ -11,23 +11,42 @@ from lib.pylabyk import np2
 from lib.pylabyk import argsutil
 from lib.pylabyk.numpytorch import npy, npys
 
-#%%
-def hist_ch_rt(ch, rt, n_ch=2, nt=None, dt=1/75, to_plot=True):
-    """
+class Decision(object):
+    def __init__(
+            self,
+            max_t=5,
+            dt=1/75,
+            n_ch=2,
+    ):
+        self.max_t = max_t
+        self.dt = dt
+        self.n_ch = n_ch
 
-    @param ch: ch[trial]
-    @param rt: rt[trial]
-    @param n_ch:
-    @param nt:
-    @return:
-    """
-    if nt is None:
-        nt = np.amax(rt) + 1
-    n = npg.aggregate(np2.cat([ch, rt]), 1., 'sum', [n_ch, nt])
-    t = np.arange(nt) * dt
+    @property
+    def nt(self):
+        return int(self.max_t // self.dt)
 
-    if to_plot:
-        h = plt.plot(t, n.T)
-    else:
-        h = None
-    return n, t, h
+    @property
+    def t(self):
+        return self.dt * np.arange(self.nt)
+
+    def hist_ch_rt(self, ch, rt, to_plot=True):
+        """
+
+        @param ch: ch[trial]
+        @param rt: rt[trial]
+        @param n_ch:
+        @param nt:
+        @return:
+        """
+        n = npg.aggregate(np2.cat([ch, rt]), 1., 'sum',
+                          [self.n_ch, self.nt])
+
+        if to_plot:
+            h = plt.plot(self.t, n.T)
+        else:
+            h = None
+        return n, h
+
+    def ev_for_ch(self, ev, ch, rt=None):
+        pass
