8329d7bacb18c154e105ed04f8dbe2302d667551
YulKang <yul.kang.on@gmail.com>
2019-12-19 14:58:56 +0000

    added taking residual and cumsum

--- decisionplot.py
+++ decisionplot.py
@@ -30,7 +30,11 @@ class Decision(object):
     def t(self):
         return self.dt * np.arange(self.nt)
 
-    def hist_ch_rt(self, ch, rt, to_plot=True):
+    def hist_ch_rt(
+            self, ch, rt,
+            to_plot=True,
+            normalize='density',
+    ):
         """
 
         @param ch: ch[trial]
@@ -39,8 +43,16 @@ class Decision(object):
         @param nt:
         @return:
         """
-        n = npg.aggregate(np2.cat([ch, rt]), 1., 'sum',
-                          [self.n_ch, self.nt])
+        n = npg.aggregate(
+            np2.cat([ch, np.round(rt / self.dt).astype('long')]),
+            1., 'sum', [self.n_ch, self.nt])
+
+        if normalize == 'density':
+            n = n / np.sum(n) / self.dt
+        elif normalize == 'None':
+            pass
+        else:
+            raise ValueError('Unsupported normalize=%s' % normalize)
 
         if to_plot:
             h = plt.plot(self.t, n.T)
@@ -48,5 +60,51 @@ class Decision(object):
             h = None
         return n, h
 
-    def ev_for_ch(self, ev, ch, rt=None):
-        pass
+    def ev_for_ch(
+            self, ev, ch,
+            rt=None,
+            summary_across_trials='mean_residual',
+            summary_within_trial='None',
+            t_align=None,
+            t_plot=None,
+            to_plot=True
+    ):
+        """
+        @param ev: [trial, it]
+        @param ch: [trial]
+        @param t_align: scalar or [trial]
+        @param t_st: scalar or [trial]
+        @param t_en: scalar or [trial]
+        @param to_plot:
+        @return: ev_for_ch[it], e_ev_for_ch[it]
+        """
+
+        n_tr = ev.shape[0]
+        ev = ev.copy()
+        if rt is not None:
+            irt = (rt / self.dt).astype('long')
+            for tr in range(n_tr):
+                ev[tr, irt:] = np.nan
+
+        ev = ev - np.nanmean(ev, 0, keepdims=True)
+        if summary_within_trial == 'None':
+            pass
+        elif summary_within_trial == 'cumsum':
+            ev = np.cumsum(ev, 1)
+        else:
+            raise ValueError('Unsupported summary_within_trial=%s' %
+                             summary_within_trial)
+
+        ev_for_ch = np.nanmean(ev * np.sign(ch[:, None] - 0.5), 0)
+        e_ev_for_ch = np2.nansem(ev * np.sign(ch[:, None] - 0.5), 0)
+
+        if t_plot is None:
+            t_plot = self.t
+        if t_align is not None:
+            raise NotImplementedError()
+
+        if to_plot:
+            plt2.errorbar_shade(t_plot, ev_for_ch, e_ev_for_ch)
+            plt.plot(t_plot, ev_for_ch)
+
+        return ev_for_ch, e_ev_for_ch
