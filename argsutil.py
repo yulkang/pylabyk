0303694a5b8c43ff3366531a97c0d51ff5364580
YulKang <yul.kang.on@gmail.com>
2020-02-17 16:45:52 +0000

    added skip_None option to argsutil.kwdef

--- argsutil.py
+++ argsutil.py
@@ -8,6 +8,7 @@ Created on Fri Oct 12 10:58:48 2018
 
 from collections import OrderedDict as odict
 
+
 def varargin2props(obj, kw, skip_absent=True, error_absent=False):
     keys0 = obj.__dict__.keys()
     for key in kw.keys():
@@ -17,6 +18,7 @@ def varargin2props(obj, kw, skip_absent=True, error_absent=False):
             raise AttributeError('Attribute %s does not exist!' % key)
         elif not skip_absent:
             obj.__dict__[key] = kw[key]
+
 
 def kwdefault(kw_given, **kw_default):
     """
@@ -33,9 +35,11 @@ def kwdefault(kw_given, **kw_default):
         kw_default[k] = kw_given[k]
     return kw_default
 
+
 def kwdef(kw_given, kw_def=None,
           sort_merged=True,
           sort_def=False, sort_given=False,
+          skip_None=False,
           def_bef_given=True):
     """
     To ensure the order is preserved, use odict or [(key:value), ...]
@@ -59,20 +63,26 @@ def kwdef(kw_given, kw_def=None,
             # raise Warning('def_bef_given=True is ignored when sort_merged=True')
         for k in kw_given:
             kw_def[k] = kw_given[k]
-        return odict(sorted(kw_def.items()))
-
-    if sort_def:
-        kw_def = odict(sorted(kw_def.items()))
-    if sort_given:
-        kw_given = odict(sorted(kw_given.items()))
-    if def_bef_given:
-        for k in kw_given:
-            kw_def[k] = kw_given[k]
-        return kw_def
+        kw_merged = odict(sorted(kw_def.items()))
     else:
-        for k in kw_def:
-            kw_given[k] = kw_def[k]
-        return kw_given
+        if sort_def:
+            kw_def = odict(sorted(kw_def.items()))
+        if sort_given:
+            kw_given = odict(sorted(kw_given.items()))
+        if def_bef_given:
+            kw_merged = kw_def
+            for k in kw_given:
+                kw_merged[k] = kw_given[k]
+        else:
+            kw_merged = kw_given
+            for k in kw_def:
+                kw_merged[k] = kw_def[k]
+    if skip_None:
+        k_to_pop = [k for k, v in kw_merged.items() if v is None]
+        for k in k_to_pop:
+            kw_merged.pop(k)
+    return kw_merged
+
 
 def kwdefs(kws, **kwargs):
     if type(kws) in {dict, odict}:
