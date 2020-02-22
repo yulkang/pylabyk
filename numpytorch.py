98a42dcb9be8eb17218eb463a677687824a7ee7f
YulKang <yul.kang.on@gmail.com>
2020-02-22 16:16:38 +0000

    npy() now leaves int or float unchanged

--- numpytorch.py
+++ numpytorch.py
@@ -110,7 +110,8 @@ def numpy(v):
         return v.clone().detach().numpy()
     except AttributeError:
         # try:
-        assert isinstance(v, np.ndarray) or sparse.isspmatrix(v)
+        assert isinstance(v, np.ndarray) or sparse.isspmatrix(v) \
+            or isinstance(v, int) or isinstance(v, float)
         return v
         # except AssertionError:
         #     return np.array(v)
