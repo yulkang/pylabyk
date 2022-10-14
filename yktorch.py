900c3c0c51e289f24f7a6b2f102fd6fdfe3b5748
YulKang <yul.kang.on@gmail.com>
2022-10-14 16:06:46 +0100

    removed superfluous print()

--- yktorch.py
+++ yktorch.py
@@ -902,18 +902,18 @@ class BoundedModule(nn.Module):
 
     def freeze_(self):
         """Freeze all parameters (set requires_grad=False)"""
-        print('use requires_grad_(False) instead!')
+        # print('use requires_grad_(False) instead!')
         self.set_requires_grad_(False)
         return self
 
     def unfreeze_(self):
         """Unfreeze all parameters (set requires_grad=True)"""
-        print('use requires_grad_(True) instead!')
+        # print('use requires_grad_(True) instead!')
         self.set_requires_grad_(True)
         return self
 
     def set_requires_grad_(self, requires_grad: bool):
-        print('use requires_grad_() instead!')
+        # print('use requires_grad_() instead!')
         npt.set_requires_grad(self, requires_grad)
 
 
