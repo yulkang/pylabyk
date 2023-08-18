204188cf9a6f62fadb704da8c3574868e8270b77
Yul Kang <yulkang@users.noreply.github.com>
2023-08-18 12:21:45 +0200

    added np2.intersection_lines

--- np2.py
+++ np2.py
@@ -1630,6 +1630,31 @@ def intersect(
            & (ccw(c, d, a) * ccw(c, d, b) <= 0)
 
 
+def intersection_lines(line1, line2):
+    """
+    Return the coordinates of the intersection of two lines
+    from https://stackoverflow.com/a/20677983/2565317
+    :param line1: [(x, y), (x, y)]
+    :param line2: [(x, y), (x, y)]
+    :return: x_intersect, y_intersect
+    """
+    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+
+    def det(a, b):
+        return a[0] * b[1] - a[1] * b[0]
+
+    div = det(xdiff, ydiff)
+    if div == 0:
+       raise Exception('lines do not intersect')
+
+    d = (det(*line1), det(*line2))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
+    return x, y
+
+
+
 def lineseg_dists(p, a, b):
     """
     Cartesian distance from point to line segment
